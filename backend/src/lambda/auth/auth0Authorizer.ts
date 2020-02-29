a607ead3dbd8a453284f62048492e3ee4d22fdce
Robert Pullan <59033909+Titan-Programmer@users.noreply.github.com>
2020-02-29 18:51:44 +0000

    Removed comments no longer required.

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -8,27 +8,8 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-const certificate = `-----BEGIN CERTIFICATE-----
-MIIDDTCCAfWgAwIBAgIJL7DOP9pd4pTAMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV
-BAMTGWRldi1mNWsyNGs2Mi5ldS5hdXRoMC5jb20wHhcNMjAwMTMxMjM0NTA0WhcN
-MzMxMDA5MjM0NTA0WjAkMSIwIAYDVQQDExlkZXYtZjVrMjRrNjIuZXUuYXV0aDAu
-Y29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyjsEcudXooSMssD9
-NVeVx0tY5JaaU/xGpPJ2oiiykao4a6Lc4SvxpFKPbGWcFsHuKAU3+dhL/tTiOi16
-XJ6kH06vMCeY7KHb0ytVZ+8SrincjRrCNRuersTv1qY6okvUKBoxNgn0/EjAkhxo
-e4Ii+vJ/C+WV+1B87N2HhXub513gppEY66bHdQsjwXuHLGa/noACZxWm8Y4sd5Rm
-Vkl7Y9S/GUHEETPYIYVn71MhFNiESqGDaxMuMIL3g12T8wtuddSolUNtyAhQhq6Y
-k1c1xq8gR+zVrB9tAey0RbQ9wlJrXTp3oUomFd/A7X7FvwJNvYGsFGqxNAoVJSph
-fM52xQIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBRTH/rvdK5O
-+EgKgT7ahuzJ+gpdZTAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEB
-AKI9eVrk6qkWtRyJ3Ygjgi+UCYWuJbjhU4K4kZWe8qM91Z+tRc/KlmDSGy/bcrJ4
-8QiLBMUDTLOdMl9T9Qw123xux/j62re7A9MZjAmsnMqJNhP16kBcQlhl0GpFvG5v
-bUt8GlVEkhF97tGlRZRPF5qUxmYoBj+wLSisk9xYxHZUl5YKhZ+lMm44SIZWdRJ1
-SHx4nCwaFsWM24Jz7ddI6m5XO5DIoIa0+06zyaRifmSWRNni4/Qj72+CQMUCq/CI
-2ZBtE1AhKT34sZVmjgCZmp30xYWQ0lgP/MgxafVCnYHDOScjvSszJhapOQ0kmF6Z
-G6BbLBwRNQp8id/gVouZANo=
------END CERTIFICATE-----`
-
-// const jwksUrl = 'https://dev-f5k24k62.eu.auth0.com/.well-known/jwks.json'
+// Enter your certificate however, this is bad practice so do this programmatically.
+const certificate = ''
 
 export const handler = async ( event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
@@ -75,8 +56,6 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   if (!jwt) {
     throw new Error('Invalid Token') 
   } 
-
-  // const certificate = await Axios.get(jwksUrl)  
 
   return verify(token, certificate, { algorithms: ['RS256'] }) as JwtPayload
 }
