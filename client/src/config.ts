7dd372f43c5d8206097b348625c9c9c972fe567f
Robert Pullan <59033909+Titan-Programmer@users.noreply.github.com>
2020-02-29 18:49:19 +0000

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,9 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it - complete
+// Your apiID and api Endpoint will need adding here.
 const apiId = ''
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+export const apiEndpoint = ''
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map - complete
+  // You need to create an Auth0 application and copy values from it into this map
   domain: '',            // Auth0 domain
   clientId: '',          // Auth0 client id
   callbackUrl: ''
